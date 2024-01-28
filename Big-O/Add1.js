743539902d04aaf45ab425d597f6b35b51702b55
Javedmv <javedmv777@gmail.com>
2024-01-28 12:18:47 +0530

    divide and conquer

--- Big-O/Add1.js
+++ Big-O/Add1.js
@@ -6,9 +6,4 @@ function addUpTon(n){
     return total
 };
 
-var t1 = performance.now();
-addUpTon(1000000000);
-var t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds. Add1`)
-
-// console.log(addUpTon(6));
+console.log(addUpTon(3));
