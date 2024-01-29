7c78a6072dad7fbfb92e194f1dfd76eaca11b496
Javedmv <javedmv777@gmail.com>
2024-01-29 21:45:40 +0530

    Set

---
+++ linkedList/singleLinkedList/linkedListSet.js
@@ -0,0 +1,97 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+    }
+} 
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        var newNode = new Node(val)
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    pop(){
+        if(!this.head)return undefined;
+        var current = this.head;
+        var newTail = current;
+        while(current.next){
+            newTail = current;
+            current = newTail.next;
+        }
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if(this.length === 0){
+            this.head = null;
+            this.tail = null;
+        }
+        return current
+    }
+    shift(){
+        if(!this.head)return undefined;
+        var oldHead = this.head;
+        this.head = oldHead.next
+        this.length--
+        if(this.length === 0){
+            this.tail = null
+        }
+        return oldHead
+    }
+    unShift(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode
+        }
+        this.length++;
+        return this;
+    }
+    Get(index){
+        if(index < 0 || index >= this.length)return null;
+        var counter = 0;
+        var current = this.head;
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        return current;
+    }
+    Set(index,val){
+        if(index < 0 || index >= this.length)return null;
+        var counter = 0;
+        var current = this.head
+        while(counter !== index){
+            current = current.next;
+            counter++;
+        }
+        current.value = val;
+        return this;
+    }
+}
+
+var list = new SinglyLinkedList()
+list.push(9)
+console.log(list);
+list.unShift(5)
+console.log(list);
+list.push(3)
+list.push(4)
+list.push(5)
+console.log(list);
+list.Set(0,"hello")
+console.log(list);
