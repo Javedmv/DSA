c3bb266468dd8eb4a26c7aa6f6be4833e573aa5b
Javedmv <javedmv777@gmail.com>
2024-01-30 14:51:03 +0530

    shift

--- linkedList/doubly Linked List/shift.js
+++ linkedList/doubly Linked List/shift.js
@@ -5,11 +5,12 @@ class Node{
         this.prev = null;
     }
 }
+
 class doublyLinkedList{
     constructor(){
         this.head = null;
         this.tail = null;
-        this.length = 0;
+        this.length = 0;         
     }
     push(val){
         var newNode = new Node(val)
@@ -21,21 +22,35 @@ class doublyLinkedList{
             newNode.prev = this.tail;
             this.tail = newNode
         }
-        this.length++;
+        this.length++
         return this;
     }
     pop(){
         if(!this.head) return undefined;
-        var oldTail = this.tail;
-        if(this.length === 1 ){
+        var oldTail = this.tail
+        if(this.length === 1){
             this.head = null;
             this.tail = null;
         }else{
-            this.tail = oldTail.prev
+            this.tail = oldTail.prev;
             this.tail.next = null;
             oldTail.prev = null;
         }
         this.length--;
         return oldTail
+    }
+    shift(){
+        if(!this.head) return undefined;
+        var oldHead = this.head
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;  
+        }
+        this.length--;
+        return oldHead;
     }
 }
