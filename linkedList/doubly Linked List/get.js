aa0ef955060471b1b21b97898bb40ea85b92ba33
Javedmv <javedmv777@gmail.com>
2024-01-30 15:10:02 +0530

    get

---
+++ linkedList/doubly Linked List/get.js
@@ -0,0 +1,79 @@
+class Node{
+    constructor(val){
+        this.value = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class doublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;         
+    }
+    push(val){
+        var newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode
+        }
+        this.length++
+        return this;
+    }
+    pop(){
+        if(!this.head) return undefined;
+        var oldTail = this.tail
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.tail = oldTail.prev;
+            this.tail.next = null;
+            oldTail.prev = null;
+        }
+        this.length--;
+        return oldTail
+    }
+    shift(){
+        if(!this.head) return undefined;
+        var oldHead = this.head
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;  
+        }
+        this.length--;
+        return oldHead;
+    }
+    unShift(val){
+        var newNode = new Node(val)
+        if(this.length === 0){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+    Get(index){
+        if(index < 0 || index >= this.length)return null;
+        var count = 0;
+        var current = this.head;
+        while(count !== index){
+            count++;
+            current = current.next;
+        }
+        return current
+    }
+}
