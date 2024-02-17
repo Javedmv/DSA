90fa9d45929d09c62805104c0a92d5976f7a686c
Javedmv <javedmv777@gmail.com>
2024-02-17 13:09:23 +0530

    bubble sorts

--- sortingAlgorithms/bubbleSort.js
+++ sortingAlgorithms/bubbleSort.js
@@ -1,16 +1,16 @@
 function bubbleSort(arr){
-    let swapped;
-    for(let i = arr.length -1 ; i > 0 ; i++){
-        swapped = true;
+    let noSwaps;
+    for(let i = arr.length ; i > 0 ; i++){
+        noSwaps = true;
         for(let j = 0 ; j < i - 1;j++){
             if(arr[j] > arr[j+1]){
                 let temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
-                swapped = false;
+                noSwaps = false;
             }
         }
-        if(swapped)break;
+        if(noSwaps)break;
     }
 };
 var arr = [5,6,3,2,1,7,8,99,7,6,55];
