096a39d305ea0190333c7ef728e0ca29ed30f04c
Javedmv <javedmv777@gmail.com>
2024-02-22 15:03:03 +0530

    stack using array

---
+++ stack & queue/stackUsingArray.js
@@ -0,0 +1,23 @@
+class stack{
+    constructor(){
+        this.item = [];
+    }
+    push(e){
+        this.item.push(e);
+    }
+    pop(){
+        if(this.item.length === 0){
+            return "underflow - stack is empty"
+        }
+        this.item.pop();
+    }
+    peek(){
+        if(!this.isEmpty()){
+            return this.item[0];
+        }
+        return 'Stack is empty'
+    }
+    isEmpty(){
+        return this.item.length == 0
+    }
+}
