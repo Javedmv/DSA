45606e060831f0905bd50cc944d51efb70e21a3e
Javedmv <javedmv777@gmail.com>
2024-02-25 17:43:18 +0530

    added Breadth first search

--- Binary-search-tree/BST.js
+++ Binary-search-tree/BST.js
@@ -51,6 +51,20 @@ class BinarySearchTree{
         if(!found)return undefined;
         return curr;
     }
+    BFS(){
+        var node = this.root;
+        var data = [];
+        var queue = [];
+        
+        queue.push(node)
+        while(queue.length){
+            node = queue.shift();
+            data.push(node)
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right;
+        }
+        return data;
+    }
 }
 
 var s = new BinarySearchTree();
