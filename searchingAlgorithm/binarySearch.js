7446878c34d13830286a3150ecc38cf22bb48ea9
Javedmv <javedmv777@gmail.com>
2024-05-30 11:40:09 +0530

    updated

--- searchingAlgorithm/binarySearch.js
+++ searchingAlgorithm/binarySearch.js
@@ -1,8 +1,7 @@
 function binarySearch(arr,val){
-    if(arr.length === 0) return -1
-    start = 0
-    end = arr.length -1
-    middle = Math.floor((start + end / 2));
+    let start = 0
+    let end = arr.length -1
+    let middle = Math.floor((start + end / 2));
     while(arr[middle] !== val && start <= end){
         if(val < arr[middle]) end = middle-1
         else start = middle + 1
